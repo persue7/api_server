5c257a1ae8c502b2981b8d7e365371b3878ba4a5
persue77 <626634851@qq.com>
2021-12-03 22:16:46 +0800

    day57晚上

--- router/artcate.js
+++ router/artcate.js
@@ -18,10 +18,12 @@ router.get('/cates', artCate_handler.getArtCates)
 router.post('/addcates', expressJoi(Reg.add_cate_schema), artCate_handler.addArticleCates)
 
 // 根据 Id 删除文章分类的路由
-// router.get('/deletecate/:id', expressJoi(delete_cate_schema), artCate_handler.deleteCateById)
+router.get('/deletecate/:id', expressJoi(Reg.delete_cate_schema), artCate_handler.deleteCateById)
+
 // 根据 Id 获取文章分类的路由
-// router.get('/cates/:id', expressJoi(get_cate_schema), artCate_handler.getArtCateById)
+router.get('/cates/:id', expressJoi(Reg.get_cate_schema), artCate_handler.getArtCateById)
+
 // 根据 Id 更新文章分类的路由
-// router.post('/updatecate', expressJoi(update_cate_schema), artCate_handler.updateCateById)
+router.post('/updatecate', expressJoi(Reg.update_cate_schema), artCate_handler.updateCateById)
 
 module.exports = router
